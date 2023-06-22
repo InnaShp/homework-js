a7f3707c867eda16fc06c146f4d9f58305cb6c62
InnaShp <107990414+InnaShp@users.noreply.github.com>
2023-06-22 22:38:04 +0300

    Added solution for point 5 task 2

--- js/task_2.js
+++ js/task_2.js
@@ -43,15 +43,40 @@ function showResultsTask2() {
   const firstSide = +prompt('Enter the fist side of triangle, please.');
   const secondSide = +prompt('Enter second side of triangle, please.');
   const thirdSide = +prompt('Enter third side of triangle, please.');
-  if (isNaN(firstSide) || isNaN(secondSide) || isNaN(thirdSide) || firstSide <= 0 || secondSide <= 0 || thirdSide <= 0) {
-    console.log('Incorrect data.');
-  } else if (firstSide + secondSide > thirdSide && firstSide + thirdSide > secondSide && secondSide + thirdSide > firstSide) {
-    const p = (firstSide + secondSide + thirdSide) / 2;
-    const areaOfTriangle = Math.sqrt(p * (p - firstSide) * (p - secondSide) * (p - thirdSide));
-    console.log('Area of triangle is ' + areaOfTriangle.toFixed(3));
-  } else console.log('The triangle with the given sides does not exist.');
+
+  function calculateTriangleArea(a, b, c) {
+    if (isNaN(a) || isNaN(b) || isNaN(c) || a <= 0 || b <= 0 || c <= 0) {
+      console.log('Incorrect data.');
+    } else if (a + b > c && a + c > b && b + c > a) {
+      const p = (a + b + c) / 2;
+      const areaOfTriangle = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+      console.log('Area of triangle is ' + areaOfTriangle.toFixed(3));
+      const isRightTriangle = a ** 2 + b ** 2 === c ** 2;
+      console.log(`This triangle is ${isRightTriangle ? '' : 'not '}a right triangle.`);
+    } else console.log('The triangle with the given sides does not exist.');
+  }
+  calculateTriangleArea(firstSide, secondSide, thirdSide);
 
   //point 5
+  const currentTime = new Date();
+  const currentHour = currentTime.getHours();
+  let greeting = null;
+  switch (true) {
+    case (currentHour >= 23 || currentHour < 5):
+      greeting = "Доброї ночі";
+      break;
+    case (currentHour >= 5 && currentHour < 11):
+      greeting = "Доброго ранку";
+      break;
+    case (currentHour >= 11 && currentHour < 17):
+      greeting = "Доброго дня";
+      break;
+    default:
+      greeting = "Доброго вечора";
+  }
+  console.log(greeting);
+
+  //point 6
   const arr = [4, 5, 2, 1, 6, 5, 3, 5, 2, 5];
   const data = arr.reduce((acc, num) => {
     acc[num] = (acc[num] || 0) + 1;
@@ -66,7 +91,7 @@ function showResultsTask2() {
   console.log(mostFrequentNumber);
   console.log(newArray);
 
-  //point 6
+  //point 7
   const pricePer100Lines = 50;
   const eachThirdDelay = 20;
   const linesPerIncome = 100
@@ -74,13 +99,12 @@ function showResultsTask2() {
   function showLinesCount() {
     const expectedIncome = +prompt('What income do you expect to have?');
     const delayCount = +prompt('How many times have you been delayed?');
-    const baseLines = Math.ceil(expectedIncome/pricePer100Lines)* linesPerIncome;
-    const penaltyLines = delayCount >= 3 ? Math.ceil(Math.floor(delayCount/3*eachThirdDelay)/pricePer100Lines) * linesPerIncome : 0;
+    const baseLines = Math.ceil(expectedIncome / pricePer100Lines) * linesPerIncome;
+    const penaltyLines = delayCount >= 3 ? Math.ceil(Math.floor(delayCount / 3 * eachThirdDelay) / pricePer100Lines) * linesPerIncome : 0;
     const linesCount = baseLines + penaltyLines;
     console.log(`You should write ${linesCount} lines of code.`);
   }
   showLinesCount();
 }
-
 
 
