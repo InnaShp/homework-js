0b89744ae71f6b1c0632f91d660aed4b602dace6
InnaShp <107990414+InnaShp@users.noreply.github.com>
2023-07-03 14:23:32 +0300

    Added solution for point 1 task 6

---
+++ js/task_6.js
@@ -0,0 +1,11 @@
+//point 1
+function showNumbers() {
+  const list = document.getElementById('list');
+  const firstElement = list.firstElementChild.textContent;
+  const lastElement = list.lastElementChild.textContent;
+  const secondElement = list.children[1].textContent;
+  const fourthElement = list.children[3].textContent;
+  const thirdElement = list.children[2].textContent;
+  alert(`${firstElement}, ${lastElement}, ${secondElement}, ${fourthElement}, ${thirdElement}`);
+}
+showNumbers();
